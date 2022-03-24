9fd3e1134518ecc6de0d40171341a3b173b5a063
StasiSie <mail.koshaksa@gmail.com>
2022-03-24 20:04:38 +0300

    first alert

--- AuthorisationApp/ViewController.swift
+++ AuthorisationApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func passwordAlert() {
+        let alert = UIAlertController(title: "Forfot password?",
+                                      message: "Here's your password: 12345",
+                                      preferredStyle: .alert)
+        self.present(alert, animated: true)
+    }
+    
 }
 
